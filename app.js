808ab83c92a43b24c3112261e24a8a7acc1a9168
ManavPatni <developer.mnvpatni@gmail.com>
2025-05-12 23:48:50 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const staffRoutes = require('./routes/staffRoutes');
 const cron = require('node-cron');
@@ -7,12 +8,55 @@ const db = require('./db/database');
 
 const app = express();
 
+// port from environment variables or default to 3000
+const PORT = process.env.PORT || 3000;
+
+// allowed origins
+const allowedOrigins = [
+  'http://127.0.0.1:8000', // Local development
+  'http://localhost:8000',  // Alternative local development
+  'https://ariessportswear.com', // Production
+  'https://admin.ariessportswear.com' // Admin subdomain
+];
+
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Mount authentication routes
+//Configure CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true
+}));
+
+// Routes
 app.use('/user', userRoutes);
 app.use('/staff', staffRoutes);
+
+// Health check endpoint
+app.get('/ping', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    code: 200
+  });
+});
+
+// Custom 404 handler for undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: 'Requested resource not found'
+  });
+});
 
 // Schedule a daily cron job to clean up database
 cron.schedule('0 0 * * *', async () => {
@@ -36,27 +80,6 @@ cron.schedule('0 0 * * *', async () => {
   } finally {
     client.release();
   }
-});
-
-
-// Define the port from environment variables or default to 3000
-const PORT = process.env.PORT || 3000;
-
-// Health check endpoint
-app.get('/ping', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    code: 200
-  });
-});
-
-// Custom 404 handler for undefined routes
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: 'error',
-    code: 404,
-    message: 'Requested resource not found'
-  });
 });
 
 // Start the server
