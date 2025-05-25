4b6ecf54e3c6d6413a1ade67c98b8f2712b73db1
ManavPatni <developer.mnvpatni@gmail.com>
2025-05-25 21:34:18 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,26 +9,28 @@ const router = express.Router();
 // Setup multe
 const upload = multer({ storage: multer.memoryStorage() });
 
-// Protected Routes
+// Public Routes
 // Category
 router.get('/categories', categoryController.getCategories);
+// Products
+router.get('/products', productController.getAllVariants);
+router.get('/products/filter', productController.getFilteredVariants);
+router.get('/product/:id', productController.getProductById);
+
+// Protected Routes
+// Category
 router.post('/category', authenticateToken, categoryController.addCategory);
 router.put('/category/', authenticateToken, categoryController.updateCategory);
 router.delete('/category/:id', authenticateToken, categoryController.deleteCategory);
-
 // Sub Category
 router.post('/sub-category', authenticateToken, categoryController.addSubCategory);
 router.put('/sub-category/', authenticateToken, categoryController.updateSubCategory);
 router.delete('/sub-category/:id', authenticateToken, categoryController.deleteSubCategory);
-
 // Products
 router.post('/product', authenticateToken, upload.any(), productController.addProduct);
 router.put('/product/:id', authenticateToken, productController.updateProduct);
-router.put('/variant/:id', authenticateToken, productController.updateVariant);
+router.put('/product/variant/:id', authenticateToken, productController.updateVariant);
 router.delete('/product/:id', authenticateToken, productController.deleteProduct);
-router.delete('/variant/:id', authenticateToken, productController.deleteVariant);
-router.get('/products', productController.getAllVariants);
-router.get('/products/filter', productController.getFilteredVariants);
-router.get('/product/:id', productController.getProductById);
+router.delete('/product/variant/:id', authenticateToken, productController.deleteVariant);
 
 module.exports = router;
