f373d10ba1b1d70fdecfe3365e67df589d7b8875
ManavPatni <developer.mnvpatni@gmail.com>
2025-07-03 23:37:44 +0530

    Add Logout function and routes for user and staff

--- controllers/staff/authController.js
+++ controllers/staff/authController.js
@@ -38,7 +38,24 @@ const refreshToken = async (req, res) => {
   res.json({ accessToken, refreshToken: newRefreshToken });
 };
 
+const logout = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(400).json({ message: 'refreshToken is required' });
+  }
+
+  try {
+    await tokenUtils.deleteRefreshToken(RefreshToken, refreshToken);
+    return res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Logout error:', error);
+    return res.status(500).json({ message: 'Server error during logout' });
+  }
+};
+
 module.exports = {
   login, 
-  refreshToken 
+  refreshToken,
+  logout
 };
--- controllers/user/authController.js
+++ controllers/user/authController.js
@@ -1,3 +1,4 @@
+const db = require('../../db/database');
 const User = require('../../models/user/userModel');
 const RefreshToken = require('../../models/user/userRefreshTokenModel');
 const bcrypt = require('bcryptjs');
@@ -116,9 +117,26 @@ const refreshToken = async (req, res) => {
   res.json({ accessToken, refreshToken: newRefreshToken });
 };
 
+const logout = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(400).json({ message: 'refreshToken is required' });
+  }
+
+  try {
+    await tokenUtils.deleteRefreshToken(RefreshToken, refreshToken);
+    return res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Logout error:', error);
+    return res.status(500).json({ message: 'Server error during logout' });
+  }
+};
+
 module.exports = { 
   sendOtp,
   register, 
   login, 
-  refreshToken 
+  refreshToken,
+  logout
 };
